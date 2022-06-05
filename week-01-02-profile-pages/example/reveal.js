33e5228f64cc62fe0302b74dc654791f56c4de03
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2022-06-05 20:01:12 +1000

    Code from Loom tutorial

--- week-01-02-profile-pages/example/reveal.js
+++ week-01-02-profile-pages/example/reveal.js
@@ -1,24 +1,30 @@
 function revealSelfie () {
   const selfie = document.getElementById('selfie')
 
-  if (selfie.style.display === 'none') {
+  // Explanation: Gets active value of display property of element
+  // 'selfie', then checks if it's 'none'.
+  // WHY NOT x.style.display? It's empty initially - accessing
+  // this property checks for values that have been set
+  // programatically.
+  if (window.getComputedStyle(selfie).display === 'none') {
     selfie.style.display = 'block'
     console.log('Selfie is now visible! 😸')
 
+    // Explanation: Get coordinates of element 'selfie' relative
+    // to viewport.
     const selfieBounds = document
       .getElementById('selfie')
       .getBoundingClientRect()
 
     emojisplosion({
       position: {
-        x: selfieBounds.left - window.innerWidth / 2 + 100,
+        // Adjust position to center of image
+        x: selfieBounds.left - window.innerWidth / 2 + 110,
         y: selfieBounds.top - 100
       }
     })
   } else {
     selfie.style.display = 'none'
-    console.log('Selfie is hidden! 😿')
+    console.log('Selfie is not visible! 😿')
   }
-
-  console.log(selfieBounds.top)
 }
